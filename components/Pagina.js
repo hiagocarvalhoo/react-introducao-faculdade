8dccbe5f18304fdd20c256f442a38df639e372fc
Hiago Carvalho <hiaagocarvalho@gmail.com>
2023-04-06 19:51:10 -0300

    add page carros

--- components/Pagina.js
+++ components/Pagina.js
@@ -8,13 +8,15 @@ const Pagina = (props) => {
     return (
         <>
             <Cabecalho />
-            <div className='bg-secondary py-3 text-white text-center mb-3'>
+            <div className='bg-secondary py-3 text-white mb-3'>
                 <Container>
-                    <h1 className='display-4'>{props.titulo}</h1>
+                    <h1>{props.titulo}</h1>
                 </Container>
             </div>
 
-            {props.children}
+            <Container className='mb-5 pb-5'>
+                {props.children}
+            </Container>
 
             <div style={{ width: '100%' }} className='bg-secondary position-fixed bottom-0 py-3 text-white text-center'>
                 <p>Todos os diretos reservados®</p>
